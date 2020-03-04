9c84a5b9b832ed9c723f34dc0d9d02a9ca81dd81
phumph <phumph@gmail.com>
2020-03-03 21:42:54 -0700

    Finished full version of call_adapteds.R and wrapped in bash script.

--- scripts/bfa_bc_adaptifier_routine.sh
+++ scripts/bfa_bc_adaptifier_routine.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 # analysis pipeline for plt
 
@@ -15,4 +15,34 @@
   # outdir name
 
 # dBFA2:
-Rscript call_adapteds.R -u --exclude=CLM\|FLC4\|Stan --gens=8 --cutoff=0.05 --base_name=cutoff-5 --outdir=../data/fitness_data/fitness_calls ../data/fitness_data/fitness_estimation/dBFA2_s_03_23_18_GC_cutoff_5.csv Ancestor_YPD_2N
+Rscript call_adapteds.R -u \
+  --exclude=CLM\|FLC4\|Stan \
+  --gens=8 \
+  --cutoff=0.05 \
+  --base_name=dBFA2_cutoff-5 \
+  --outdir=../data/fitness_data/fitness_calls \
+  ../data/fitness_data/fitness_estimation/dBFA2_s_03_23_18_GC_cutoff_5.csv \
+  Ancestor_YPD_2N
+
+# hBFA1
+Rscript call_adapteds.R -u \
+  --exclude=X48Hr \
+  --gens=8 \
+  --cutoff=0.05 \
+  --base_name=hBFA1_cutoff-5 \
+  --outdir=../data/fitness_data/fitness_calls \
+  ../data/fitness_data/fitness_estimation/hBFA1_s_03_23_18_GC_cutoff_5.csv \
+  YPD_alpha
+
+# hBFA2
+# This assay is special because we do not have a neutral reference for 2N clones
+# Rscript call_adapteds.R -u \
+#   --exclude=CLM\|X08M_NaCl\|X48Hr \
+#   --gens=8 \
+#   --cutoff=0.05 \
+#   --base_name=hBFA2_cutoff-5 \
+#   --outdir=../data/fitness_data/fitness_calls \
+#   ../data/fitness_data/fitness_estimation/hBFA2_s_03_23_18_GC_cutoff_5.csv \
+#   YPD_alpha
+
+# next step: post-process hBFA1 adapted columns to flag auto-diploids, both adapted and ancestral.
--- scripts/call_adapteds.R
+++ scripts/call_adapteds.R
@@ -15,6 +15,7 @@ suppressWarnings(suppressMessages(library(tidyr)))
 suppressWarnings(suppressMessages(library(ggplot2)))
 suppressWarnings(suppressMessages(library(progress)))
 suppressWarnings(suppressMessages(library(docopt)))
+source(file.path('./adapted_functions.R'))
 
 # ------------------------- #
 # setup command line inputs #
@@ -30,12 +31,12 @@ Options:
     -h --help                     Show this screen.
     -v --version                  Show version.
     -o --outdir=<outdir>          Output directory [default: ./]
-    -b --base_name=<name>         Base name for output file [default: tmp]
+    -b --base_name=<base_name>   Base name for output file [default: tmp]
     -u --use_iva                  Flag to determine whether to use inverse variance weighted avg or arithmentic avg [default: TRUE]
     -g --gens=<gens>              Number of generations per cycle (used to divide input fitness estimates) [default: 8]
-    -c --cutoff=<pval>            P-value cutoff for outlier trimming [default: 0.001]
+    -c --cutoff=<pval>            P-value cutoff for outlier trimming [default: 0.05]
     -e --exclude=<env>...         Space-separated list of environments to exclude from neutral set calculations
-    -r --reps_iter=<sim_reps>     Number of replicate simulations for determining neutral set [default: 500]
+    -r --reps_iter=<sim_reps>     Number of replicate simulations for determining neutral set [default: 1000]
     -f --reps_final=<final_reps>  Number of replicate simulations for final neutral probability distribution [default: 1000]
 
 Arguments:
@@ -43,26 +44,23 @@ Arguments:
     neutral_col                   String denoting set of putatively neutral BCs
 ' -> doc
 
-#print(arguments)
-
 # -------------------- #
 # function definitions #
 # -------------------- #
 
-
 run_args_parse <- function(debug_status) {
-  
+
   if (debug_status == TRUE) {
     arguments <- list()
     arguments$use_iva     <- TRUE
     arguments$infile      <- "../data/fitness_data/fitness_estimation/dBFA2_s_03_23_18_GC_cutoff_5.csv"
-    arguments$outdir     <- "../data/fitness_data/fitness_calls"
+    arguments$outdir      <- "../data/fitness_data/fitness_calls"
     arguments$neutral_col <- 'Ancestor_YPD_2N'
-    arguments$name        <- 'cutoff-5'
-    arguments$reps_iter   <- 500
+    arguments$base_name        <- 'dBFA2_cutoff-5'
+    arguments$reps_iter   <- 1000
     arguments$reps_final  <- 1000
-    arguments$exclude     <- 'CLM|FLC4'
-    arguments$cutoff      <- 0.01
+    arguments$exclude     <- 'CLM|FLC4|Stan'
+    arguments$cutoff      <- 0.05
     arguments$gens        <- 8
   } else if (debug_status == FALSE) {
     arguments <- docopt(doc, version = 'call_adapteds v.1.0')
@@ -70,210 +68,10 @@ run_args_parse <- function(debug_status) {
   return(arguments)
 }
 
-
-OpenRead <- function(arg) {
-  if (arg %in% c("-", "/dev/stdin")) {
-    file("stdin", open = "r")
-  } else {
-    file(arg, open = "r")
-  }
-}
-
-prep_fit_matrix <- function(df, is_neutral = TRUE, means_only = FALSE, neutral_col, excludes, iva_s, gens) {
-
-  # take input counts file and generate list of 2 matrixes:
-    # (n x k) matrix of k-variate mean vectors for n barcodes
-    # (n x k) k-variate sigma vectors for n barcodes
-
-  # for debugging:
-  # excludes <- list(c('CLM'), c('FLC4'), c('Stan'))
-  # neutral_col <- 'Ancestor_YPD_2N'
-  # df = bfa_dat
-  # gens = 8
-
-  avg_type <- ifelse(iva_s == TRUE, 'iva_s', 'ave')
-
-  focal_cols <- c(grep('\\.BC',   names(df), value = T),
-                  grep('Subpool', names(df), value = T),
-                  grep('\\.R[0-9]', names(df), invert = T, value = T),
-                  grep(paste0(avg_type),  names(df), value = T))
-
-  focal_cols <- focal_cols[!grepl(paste0(unlist(excludes), collapse='|'), focal_cols)]
-
-  df <- df[ , names(df) %in% focal_cols]
-
-  # examine list of neutrals if flag present
-  if (is_neutral == TRUE) {
-    if (!is.null(neutral_col)) {
-      df <-
-        df %>%
-        dplyr::filter(Subpool.Environment == neutral_col)
-    } else {
-      stop("Please define neutral_col!")
-    }
-  }
-
-  row.names(df) <- df$Full.BC
-
-  df_mat <-
-    df %>%
-    dplyr::select(grep('iva_s$|iva_s_err$', names(df), value = T)) %>%
-    as.matrix()
-
-  # now I can remove NA, Inf values:
-  df_mat[is.infinite(df_mat)] <- NA
-  df_mat <- df_mat[complete.cases(df_mat), ]
-
-  # split into means and sigmas:
-  if (means_only == FALSE) {
-
-    df_list <- list(means  = df_mat[, grep('iva_s$', colnames(df_mat))] / gens,
-                    sigmas = df_mat[, grep('iva_s_err$', colnames(df_mat))] / gens)
-
-    return(df_list)
-
-  } else if (means_only == TRUE) {
-
-    means <- df_mat[, grep('iva_s$', colnames(df_mat))] / gens
-
-    return(means)
-  }
-}
-
-
-filter_neutral_outliers <- function(dfs, reps_iter, cutoff, init_cutoff = 0.05, GENS = 8) {
-
-  # for each barcode left out:
-  # generate predictive distribution by sampling from vector of means and stderrs
-
-  # # for debugging:
-  # dfs <- neut_list
-  #
-  means  <- dfs[["means"]]
-  sigmas <- dfs[["sigmas"]]
-
-  # # grab unique barcodes
-  # bcs <- row.names(means) %>% unique()
-  #
-  # find distance of each barcode to mean; sort by desc(distance)
-  starting_dists <- mahalanobis(means,
-                                colMeans(means),
-                                cov(means))
-
-  means <-
-    means %>%
-    cbind(starting_dists)
-
-  sigmas <-
-    sigmas %>%
-    cbind(starting_dists)
-
-  means  <- means[order(means[,   'starting_dists'], decreasing = TRUE), ]
-  sigmas <- sigmas[order(sigmas[, 'starting_dists'], decreasing = TRUE), ]
-
-  # determine quick and dirty the set I'm going to evaluate
-  # all with Chisq p-value of < init_cutoff
-
-  init_pvals <- pchisq(means[, 'starting_dists'], df = dim(means)[2] - 1, lower.tail = FALSE)
-
-  # grab barcodes with p-values <= init_cutoff:
-  candidate_outliers <- names(init_pvals[init_pvals <= init_cutoff])
-
-  # go through each candidate outlier
-  # remove it
-  # generate more robust testing distribution
-  # flag to remove given p-value
-
-  # NEED TO FINISH THIS ROUTINE:
-  # replace with while loop which progressively eliminates barcodes
-  # until stopping condition is met
-  # for (i in seq_along(candidate_outliers)) {
-  #
-  #   # remove focal barcode
-  #   means2  <- means[-i,  grep('starting_dists', colnames(means), invert = TRUE)]
-  #   sigmas2 <- sigmas[-i, grep('starting_dists', colnames(means), invert = TRUE)]
-  #   test_bc <- means[i,   grep('starting_dists', colnames(means), invert = TRUE)]
-  #
-  #   # generate multivariate normal random variables for each vector:
-  #   res_mat <- NULL
-  #
-  #   # wrap this in a function call
-  #   for (ii in seq_along(row.names(means2))) {
-  #     res_mat <- rbind(res_mat, MASS::mvrnorm(n = R, means2[ii, ], diag(sigmas2[ii, ]^2)))
-  #   }
-  #
-  #   # calculate distance of each barcode in set
-  #   D <- mahalanobis(rbind(test_bc,res_mat),
-  #                    colMeans(res_mat),
-  #                    cov(res_mat))
-  #
-  #   # generate distance of test barcode
-  #   D_test <- mahalanobis(test_bc,
-  #                         colMeans(res_mat),
-  #                         cov(res_mat))
-  #
-  #   # calculate p-value of test_bc dist
-  #   P_test <- sum(D >= D_test) / length(D)
-  # }
-
-  final_neuts <- row.names(means)[!row.names(means) %in% candidate_outliers]
-  return(final_neuts)
-}
-
-
-generate_neutral_test_distn <- function(dfs, neutrals, reps_final) {
-
-  means  <- dfs[["means"]]
-  sigmas <- dfs[["sigmas"]]
-
-  # subset to neutral list
-  means2  <-  means[row.names(means) %in% neutrals, ]
-  sigmas2 <- sigmas[row.names(sigmas) %in% neutrals, ]
-
-  # generative model of distribution of k-variate vectors
-  res_mat <- NULL
-  for (ii in seq_along(row.names(means2))) {
-    res_mat <- rbind(res_mat, MASS::mvrnorm(n = reps_final, means2[ii, ], diag(sigmas2[ii, ]^2)))
-  }
-
-  # generate testing distribution of distances to apply to all barcodes
-  D <- mahalanobis(res_mat,
-                   colMeans(res_mat),
-                   cov(res_mat))
-
-  L1 <- list(col_means = colMeans(res_mat),
-             cov_matr  = cov(res_mat),
-             distances = D)
-
-  return(L1)
-}
-
-
-flag_adapteds <- function(matr, testing_distn, cutoff) {
-
-  # takes mean vector of all barcodes, neutral test distn,
-  # and compares distance of each barcode to the neutral set.
-  # for now, does not handle missing values but simply excludes them.
-  # I'll need to update the script to do this soon.
-
-  # calculate distance of each barcode versus neutral set:
-  D <- mahalanobis(matr,
-                   testing_distn$col_means,
-                   testing_distn$cov_matr)
-
-  # for each: determine p-value within testing distribution
-  p_vals <- sapply(D, function(x) sum(testing_distn$distances >= x) / length(testing_distn$distances))
-  is_adapted <- D > quantile(testing_distn$distances, probs = 1 - cutoff)
-
-  return(data.frame(Full.BC = row.names(matr),
-                    matr,
-                    maha_dist = D,
-                    dist_pval = p_vals,
-                    is_adapted))
-}
-
 main <- function(bfa_dat, arguments) {
 
+  set.seed(12345)
+  
   # take bfa_dat
   # run it through name conversion
   cat("Preparing input file...")
@@ -286,7 +84,7 @@ main <- function(bfa_dat, arguments) {
                     gens        = as.double(arguments$gens),
                     is_neutral  = TRUE)
   cat("Done!\n")
-  cat("Detecting outliers...")
+  cat("Detecting outliers in neutral barcode set...")
 
   neutral_set <-
     fit_mats %>%
@@ -301,8 +99,35 @@ main <- function(bfa_dat, arguments) {
     fit_mats %>%
     generate_neutral_test_distn(neutrals   = neutral_set,
                                 reps_final = as.double(arguments$reps_final))
+  # write to file:
+  if (!dir.exists(file.path(arguments$outdir, arguments$base_name))) {
+    dir.create(file.path(arguments$outdir, arguments$base_name))
+  }
+  
+  saveRDS(neutral_test_distn,
+          file = file.path(arguments$outdir, arguments$base_name, 'neutral_test_distn.Rds'))
+  
+  # save plot of distribution
+  data.frame(distances = neutral_test_distn$distances) %>%
+    ggplot() +
+    geom_histogram(aes(x = distances), bins = 100) +
+    theme_minimal() +
+    theme(axis.line = element_line(),
+          plot.title = element_text(size = 9, face = 'bold')) +
+    geom_vline(xintercept = quantile(neutral_test_distn$distances, probs = 1 - as.double(arguments$cutoff)), col = 'darkorange') +
+    ggtitle(paste0("Neutral D distn (", length(neutral_set)," BCs)\n", arguments$base_name, "\ncutoff = ", arguments$cutoff)) ->
+    neutral_dist_plot
+  
+  ggsave(neutral_dist_plot,
+         filename = file.path(arguments$outdir, arguments$base_name, 'neutral_test_distn.png'),
+         device = 'png',
+         dpi = 300,
+         width = 5,
+         height = 3.5,
+         units = 'in')
+  
   cat("Done!\n")
-
+  
   cat("Determining adapted barcodes...")
   # flag input barcodes as adapted or not based on distance
   adapteds <-
@@ -341,124 +166,37 @@ main <- function(bfa_dat, arguments) {
 # MAIN #
 # ---- #
 
-infile   <- OpenRead(arguments$infile)
-
-# # FOR DEBUGGING
-# infile <- OpenRead("../data/fitness_data/fitness_estimation/dBFA2_s_03_23_18_GC_cutoff_5.csv")
-
-bfa_dat  <- read.table(infile,
-                       header = TRUE,
-                       sep = ',',
-                       stringsAsFactors = F)
-
 # run
-arguments <- run_args_parse(debug_status == TRUE)
-res_out <- main(bfa_dat, arguments = arguments)
+cat("*******************\n")
+cat("* call_adapteds.R *\n")
+cat("*******************\n\n")
 
-cat("Writing output file...")
+debug_status <- FALSE
+arguments <- run_args_parse(debug_status)
+infile    <- OpenRead(arguments$infile)
+dat <- read.table(infile,
+                 header = TRUE,
+                 sep = ',',
+                 stringsAsFactors = F)
+
+res_out <- main(dat, arguments = arguments)
 
 # write
 if (!dir.exists(arguments$outdir)) {
   dir.create(arguments$outdir)
 }
 
+outfile_path <- file.path(arguments$outdir, paste0(arguments$base_name, '_adapteds_',Sys.Date(),'.csv'))
+
+cat(sprintf("Writing output file: %s\n", outfile_path))
+
 write.table(res_out,
-            file = file.path(arguments$outdir, paste0(arguments$base_name, '_adapteds_',Sys.Date(),'.csv')),
+            file = outfile_path,
             quote = F,
             sep = ',',
             col.names = T,
             row.names = F)
 
-cat("Done!\n")
 cat("**Script completed successfully!**\n\n")
 
 ### END ###
-
-
-
-#### OBSOLETE BELOW THIS LINE ####
-
-# # grab each environment:
-# bfa_envs <-
-#   neuts %>%
-#   names() %>%
-#   grep('Subpool|BC', ., invert = T, value = T) %>%
-#   strsplit('\\.') %>%
-#   sapply(function(x) dplyr::first(x)) %>%
-#   unique()
-#
-# # run through environments and calculate leave-one-out z-score in each environment
-# res_by_env <- lapply(bfa_envs, function(x) detect_outliers(env = x, df = neuts))
-#
-# # bring scores per barcode together and generate plot of distributions
-# res_by_env_long <- do.call(rbind, res_by_env)
-#
-# # generate sum of squares to see if it has decent testing properties
-# res_by_env_long %>%
-#   dplyr::group_by(Full.BC, bfa_env) %>%
-#   dplyr::summarise_if(is.numeric, sum) ->
-#   res_by_env_sum
-#
-# # the squared z-score for each environment should be distributed as a Chi-squared with 1 df.
-# # we can therefore calculate the joint likelihood across environments and apply a cutoff:
-#
-#
-# res_by_env_long %>%
-#   dplyr::group_by(Full.BC, bfa_env) %>%
-#   dplyr::summarise(pval = pchisq(q0.500, df = 1, lower.tail = FALSE)) ->
-#   res_by_env_p
-#
-# res_by_env_p_prod <-
-#   res_by_env_p %>%
-#   dplyr::ungroup() %>%
-#   dplyr::group_by(Full.BC) %>%
-#   dplyr::summarise(pprod = prod(pval, na.rm = T)) %>%
-#   dplyr::full_join(
-#     tidyr::spread(res_by_env_p, key = 'bfa_env', value = 'pval'))
-#
-# ggplot(res_by_env_p_prod, aes(x = pprod)) +
-#   stat_ecdf() #geom_histogram(bins = 100)
-#
-#
-#
-# #### obsolete below this line ####
-#
-#
-# ggplot(res_by_env_sum, aes(x = q0.500)) + #geom_histogram(bins = 50) +
-#   geom_density() +
-#   #facet_wrap(~ bfa_env) +
-#   scale_x_continuous(limits = c(0,3))
-#
-# # try plotting
-# ggplot(res_by_env_long, aes(x = bfa_env, y = q0.500, ymin = q0.025, ymax = q0.975, group = Full.BC)) +
-#   geom_linerange(alpha = 0.1) +
-#   geom_line(alpha = 0.3) +
-#   geom_point(alpha = 0.5) +
-#   theme_minimal() +
-#   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0)) +
-#   scale_y_log10()
-#
-# # do sum across environment and see what it looks like
-# res_by_env_sum <-
-#   res_by_env_long %>%
-#   dplyr::group_by(Full.BC) %>%
-#   dplyr::summarise_if(is.numeric, sum)
-#
-# # need to define cutoff here somehow.
-# # In theory, this will be the same process as for defining adapted status, but with the Neutrals.
-# # calculate p-value under the Chisquared with df = 1 for each z_i
-# # inspect distribution of log p-values across all environments; find the product across environments
-# # Basically calculating the likelihood of the data under the hypothesis that mean(X) = 0.
-#
-# ggplot(res_by_env_sum, aes(x = q0.500)) + geom_density(bins = 1000) +
-#   scale_x_continuous(limits = c(0,50))
-#
-# # now try to implement:
-# # run on each environment; decide on cutoff for excluding outliers
-# res_test <- dplyr::arrange(res_test, q0.500)
-# res_test$Full.BC <- factor(res_test$Full.BC, levels = paste0(res_test$Full.BC))
-#
-# ggplot(res_test, aes(x = Full.BC, y = q0.500)) +
-#   geom_point() +
-#   geom_linerange(aes(ymin = q0.025, ymax = q0.975)) +
-#   theme_minimal()
